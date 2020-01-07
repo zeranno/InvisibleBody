7a5b55feba9e32b768a37858e03595569a916265
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-07 10:01:35 -0600

    Add project

---
+++ src/EC_InvisibleBody/EC.InvisibleBody.cs
@@ -0,0 +1,9 @@
+﻿using BepInEx;
+
+namespace IllusionMods
+{
+    [BepInDependency(KKAPI.KoikatuAPI.GUID)]
+    [BepInDependency(ExtensibleSaveFormat.ExtendedSave.GUID)]
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class InvisibleBody : BaseUnityPlugin { }
+}
