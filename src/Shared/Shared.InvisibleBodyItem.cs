907ed044f5cba3f1dc2f40cfaa97a7b1b94088ed
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-07 12:26:07 -0600

    Add InvisibleBodyItem template

---
+++ src/Shared/Shared.InvisibleBodyItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace IllusionMods.InvisibleBody
+{
+    class InvisibleBodyItem : MonoBehaviour
+    {
+        internal void Start()
+        {
+            PluginBase.Logger.LogMessage($"InvisibleBodyItem Start");
+        }
+    }
+}
