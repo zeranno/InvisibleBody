0cc85e9e9e6a224e37dd1540555e5cbba0401465
zeranno <zeranno4@gmail.com>
2020-01-08 03:13:51 -0700

    Update Shared.InvisibleBodyItem.cs

--- src/Shared/Shared.InvisibleBodyItem.cs
+++ src/Shared/Shared.InvisibleBodyItem.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace IllusionMods.InvisibleBody
 {
     class InvisibleBodyItem : MonoBehaviour
     {
+        /// <summary>
+        /// Sends a message to log when accessory is first added.
+        /// </summary>
         internal void Start()
         {
             PluginBase.Logger.LogMessage($"InvisibleBodyItem Start");
+        }
+
+        /// <summary>
+        /// Activates effect when accessory is on.
+        /// </summary>
+        private void OnEnable()
+        {
+            PluginBase.Logger.LogMessage($"Test On");
+        }
+
+        /// <summary>
+        /// Disables effect when accessory is off.
+        /// </summary>
+        private void OnDisable()
+        {
+            PluginBase.Logger.LogMessage($"Test Off");
         }
     }
 }
